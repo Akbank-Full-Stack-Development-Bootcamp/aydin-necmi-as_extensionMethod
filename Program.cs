0cfefa1f037efb18c4815948136253b599d51be3
Aydın Necmi AS <49839084+aydinnecmias@users.noreply.github.com>
2021-06-06 19:53:05 +0300

    Aydin Necmi AS Extension Method Homework1
    Girilen cümledeki i'leri "ö" haline çeviren .Troll() extension

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace FunnyExtension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir cümle giriniz:");
+            var cumle = Console.ReadLine();
+
+            string komik = cumle.Troll();
+
+            Console.WriteLine(komik);
+
+
+        }
+    }
+
+<<<<<<< HEAD
+    public static class Funny
+=======
+    public static class Funny 
+>>>>>>> parent of 9d0abe9 (Delete FunnyExtension directory)
+    {
+
+        public static string Troll(this string param)
+
+        {
+
+            if (param.Contains("i")) { }
+
+<<<<<<< HEAD
+
+            return param.Replace("i", "ö");
+=======
+                      
+                return param.Replace("i", "ö");
+>>>>>>> parent of 9d0abe9 (Delete FunnyExtension directory)
+
+
+
+        }
+<<<<<<< HEAD
+
+=======
+       
+>>>>>>> parent of 9d0abe9 (Delete FunnyExtension directory)
+
+    }
+}
